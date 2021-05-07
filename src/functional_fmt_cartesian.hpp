eb7330c1c134b1b8b4281b06d3eab203153da915
DFTnator <philipp.pelagejcev@physik,uni-freiburg.de>
2021-05-07 10:51:57 +0200

    Added function_fmt_cartesian.hpp with base functions

---
+++ src/functional_fmt_cartesian.hpp
@@ -0,0 +1,19 @@
+//  Project CapDFT
+//  functional_fmt_cartesian.cpp
+//  WB-Mark II functional for hard spheres in cartesian coordinates
+//  Copyright 2021 Philipp Pelagejcev
+
+#ifndef SRC_FUNCTIONAL_FMT_CARTESIAN_H_
+#define SRC_FUNCTIONAL_FMT_CARTESIAN_H_
+
+class FunctionalFMTCartesian {
+public:
+  FunctionalFMTCartesian(void);
+  ~FunctionalFMTCartesian(void);
+  void calc_derivative(void);
+  void calc_fluid_derivative(void);
+  double **get_derivative_pointer(void);
+  double **get_fluid_derivative_pointer(void);
+};
+
+#endif // SRC_FUNCTIONAL_FMT_CARTESIAN_H_
