aeca39a760ac82f776067c60391df051c9ee6333
moritz-bueltmann <moritz.bueltmann@physik.uni-freiburg.de>
2021-05-07 13:51:45 +0200

    Added blank files for species properties class and for the spherical FMT functional class.

---
+++ src/functional_fmt_spherical.cpp
@@ -0,0 +1,15 @@
+/*******************************************************************************
+* Copyright 2021 Moritz Bueltmann
+* Authors: Moritz Bueltmann <moritz.bueltmann@gmx.de>
+* Physics Department Albert-Ludwigs-Universitaet
+*******************************************************************************/
+#include "functional_fmt_spherical.hpp"
+// _____________________________________________________________________________
+FunctionalFMTSpherical::FunctionalFMTSpherical() {
+  //
+}
+// _____________________________________________________________________________
+FunctionalFMTSpherical::~FunctionalFMTSpherical() {
+  //
+}
+// _____________________________________________________________________________
