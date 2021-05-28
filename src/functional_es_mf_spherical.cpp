6aa717a9266fc1405786bb61352c6d07f4f97d6d
moritz-bueltmann <moritz.bueltmann@physik.uni-freiburg.de>
2021-05-28 12:55:03 +0200

    Added stub implementation for spherical electrostatics functional.

---
+++ src/functional_es_mf_spherical.cpp
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2021 Moritz Bültmann <moritz.bueltmann@gmx.de>
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "functional_es_mf_spherical.hpp"
+// _____________________________________________________________________________
+FunctionalESMFSpherical::FunctionalESMFSpherical() {
+  //
+}
+// _____________________________________________________________________________
+FunctionalESMFSpherical::~FunctionalESMFSpherical() {
+  //
+}
+// _____________________________________________________________________________
