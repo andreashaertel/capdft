8576bdd8e71fa724f861fca003abced59859423b
moritz-bueltmann <moritz.bueltmann@physik.uni-freiburg.de>
2022-06-23 16:29:33 +0200

    Finished electrostatic mean-field functional

--- src/functional_es_mf_spherical.cpp
+++ src/functional_es_mf_spherical.cpp
@@ -1,67 +1,229 @@
-// SPDX-FileCopyrightText: 2021 Moritz Bültmann <moritz.bueltmann@gmx.de>
+// SPDX-FileCopyrightText: 2022 Moritz Bültmann <moritz.bueltmann@gmx.de>
 // SPDX-License-Identifier: LGPL-3.0-or-later
 #include "functional_es_mf_spherical.hpp"  // NOLINT
+#include <cmath>
+// Define some natural constants
+#define ELECTRON_CHARGE 1.602176634  // *1e-19
+#define BOLTZMANN 1.38064852  // *1e-23
+#define VACUUM_PERMITIVITY 8.8541878128  // *1e-12
+#define AVOGADRO 6.02214076  // *1e23
+// _____________________________________________________________________________
+FunctionalESMFSpherical::FunctionalESMFSpherical() {
+}
 // _____________________________________________________________________________
 FunctionalESMFSpherical::FunctionalESMFSpherical(
-    const Properties& system_properties,
+    std::vector<DataFrame<1, double>>* density_profiles,
     const std::vector<Properties>& species_properties,
-    double** density_profile)
-  : density_profile(density_profile) {
-  // Clear all std::vectors
-  valencies.clear();
-  diameters.clear();
-  bulk_densities.clear();
+    const Properties& system_properties,
+    std::vector<size_t> affected_species)
+  : affected_species(affected_species),
+    density_profiles_pointer(density_profiles) {
   // Get system properties
-  system_properties.get_property("length", &length);
-  system_properties.get_property("grid count", &grid_count);
-  system_properties.get_property("temperature", &temperature);
-  system_properties.get_property("dielectric constant", &dielectric);
-  system_properties.get_property("bjerrum length", &bjerrum_length);
-  // TODO(Moritz): cases for bjerrum length and temperature + dielectric
-  // Get species properties; excludes all species without diameter
-  double diameter{0.};
-  double bulk_density{0.};
-  double valency{0.};
-  for (auto it = species_properties.begin(); it != species_properties.end();
-      ++it) {
-    if (it->get_property("valency", &valency)) {  // only species with valency
-      if (!it->get_property("bulk density", &bulk_density)) {
-        // TODO(Moritz): throw error, because species has valency, but no dens
-      } else {
-        bulk_densities.push_back(bulk_density);
-      }
-      if (!it->get_property("diameter", &diameter)) {
-        diameters.push_back(0.);
-      } else {
-        diameters.push_back(diameter);
-      }
-    }
-  }
-  // Count species that interact via the hard sphere potential
-  species_count = diameters.size();
-  // TODO(Moritz): Initialize the following objects
-  // TODO(Moritz): functional_derivative
-  // TODO(Moritz): charge_density_profile
-  // TODO(Moritz): species_potentials
+  extract_system_properties(system_properties);
+  // Get species properties; excludes all species without diameter property
+  extract_species_properties(species_properties);
+  // Initialize all data frames and update charge densities
+  initialize_all_data_frames();
+  calc_charge_densities();
+  initialize_poisson_solver();
+}
+// _____________________________________________________________________________
+FunctionalESMFSpherical::FunctionalESMFSpherical(
+    std::vector<DataFrame<1, double>>* density_profiles,
+    const std::vector<Properties>& species_properties,
+    const Properties& system_properties)
+  : FunctionalESMFSpherical(
+      density_profiles, species_properties, system_properties,
+      std::vector<size_t>(0)) {
 }
 // _____________________________________________________________________________
 FunctionalESMFSpherical::~FunctionalESMFSpherical() {
   //
 }
 // _____________________________________________________________________________
+void FunctionalESMFSpherical::extract_system_properties(
+    const Properties& system_properties) {
+  // Extract system properties directly
+  system_properties.get_property("length", &length);
+  system_properties.get_property("grid count", &grid_count);
+  // Calculate bin sizes
+  dr = length / static_cast<double>(grid_count);
+  dkr = (2. * M_PI) / (2. * static_cast<double>(grid_count+1) * dr);
+  // Calculate electrical properties of the system
+  extract_electrical_properties(system_properties);
+}
+// _____________________________________________________________________________
+void FunctionalESMFSpherical::extract_electrical_properties(
+    const Properties& system_properties) {
+  // This variable saves a number representing, which of the el. variables are
+  // available.
+  int cases{0};
+  // Get the variables
+  try {
+    if (system_properties.get_property("temperature", &temperature)) {
+      cases += 1;
+    }
+  } catch (...) {}
+  try {
+    if (system_properties.get_property("bjerrum length", &bjerrum)) {
+      cases += 2;
+    }
+  } catch (...) {}
+  try {
+    if (system_properties.get_property("dielectric constant", &dielectric)) {
+      cases += 4;
+    }
+  } catch (...) {}
+  // Auxiliary variable e^2 / (4 * \epsilon_0 * \pi * k_B)
+  double e2_by_4pi_boltz = 1e-3 * ELECTRON_CHARGE * ELECTRON_CHARGE /
+      (4. * M_PI * VACUUM_PERMITIVITY * BOLTZMANN);
+  // Calculate missing variables
+  switch (cases) {
+    case 3:  // dielectric constant missing
+      dielectric = e2_by_4pi_boltz / (bjerrum * 1e-9 * temperature);
+      break;
+    case 5:  // bjerrum length missing
+      bjerrum = 1e9 * e2_by_4pi_boltz / (dielectric * temperature);
+      break;
+    case 6:  // temperature missing
+      temperature = e2_by_4pi_boltz / (dielectric * bjerrum * 1e-9);;
+      break;
+    case 7:  // nothing missing
+      std::cerr << "FunctionalESMFSpherical::extract_electrical_properties(): ";
+      std::cerr << "\"Warning: All three electrical system properties are ";
+      std::cerr << "specified. Consistency will not be checked.";
+      std::cerr << std::endl;
+      break;
+    default:
+      std::cerr << "FunctionalESMFSpherical::extract_electrical_properties(): ";
+      std::cerr << "\"Error: Specify two properties of the following: ";
+      std::cerr << "temperature, bjerrum length, dielectric constant!\"";
+      std::cerr << std::endl;
+      exit(1);
+  }
+}
+// _____________________________________________________________________________
+void FunctionalESMFSpherical::extract_species_properties(
+    const std::vector<Properties>& species_properties) {
+  // Sort the affected species numbers
+  std::sort(affected_species.begin(), affected_species.end());
+  // Remove duplicates
+  affected_species.erase(
+      unique(affected_species.begin(), affected_species.end()),
+      affected_species.end());
+  // If no affected species were specified, find them automatically
+  double valency{0.};
+  if (affected_species.empty()) {
+    for (auto& properties : species_properties) {
+      try {
+        if (properties.get_property("valency", &valency)) {
+          affected_species.push_back(&properties - &species_properties[0]);
+        }
+      } catch(...) {}
+    }
+  }
+  // Get valencies
+  for (auto& species : affected_species) {
+    species_properties.at(species).get_property("valency", &valency);
+    valencies.push_back(valency);
+  }
+  // Count species that interact via the electrostatic forces
+  species_count = affected_species.size();
+}
+// _____________________________________________________________________________
+void FunctionalESMFSpherical::initialize_all_data_frames() {
+  charge_density_profile.~DataFrame<1, double>();
+  new(&charge_density_profile) DataFrame<1, double>(grid_count);
+  poisson_rhs.~DataFrame<1, double>();
+  new(&poisson_rhs) DataFrame<1, double>(grid_count);
+  potential.~DataFrame<1, double>();
+  new(&potential) DataFrame<1, double>(grid_count);
+}
+// _____________________________________________________________________________
+void FunctionalESMFSpherical::initialize_poisson_solver() {
+  poisson_solver = new RadialPoissonSolver(grid_count, dr, .5 * dr);
+  poisson_solver->set_radial_laplacian(NEUMANN_DIRICHLET);
+}
+// _____________________________________________________________________________
+void FunctionalESMFSpherical::calc_charge_densities() {
+  charge_density_profile.zero();
+  for (size_t i = 0; i < species_count; ++i) {
+    charge_density_profile += valencies.at(i) *
+        density_profiles_pointer->at(affected_species.at(i));
+  }
+  poisson_rhs = -4. * M_PI * bjerrum * charge_density_profile;
+}
+// _____________________________________________________________________________
+void FunctionalESMFSpherical::calc_potential() {
+  double outer_boundary{0.};
+  double inner_boundary{0.};
+  // Calculate the boundary conditions of the Poisson equation.
+  // The inner one equals 0 (Neumann) due to the radial symmetry and the outer
+  // one equals net charge divided by radial position (Dirichlet) due to Gauss'
+  // theorem. Note, that there is no external charge at the center.
+  inner_boundary = 0.;
+  outer_boundary = bjerrum * calc_net_charge() /
+      (dr * static_cast<double>(grid_count+1));
+  // Solve the radial Poisson equation numerically
+  poisson_solver->solve(
+      inner_boundary, outer_boundary, poisson_rhs.array(), potential.array());
+}
+// _____________________________________________________________________________
+double FunctionalESMFSpherical::calc_net_charge() {
+  double integral{0.};
+  double r{0.};
+  // Trapezoidal integral rule, spherical integral
+  r = static_cast<double>(0 + 1) * dr;
+  integral += .5 * r * r * charge_density_profile.at(0) * dr;
+  for (size_t i = 1; i < grid_count - 1; ++i) {
+    r = static_cast<double>(i + 1) * dr;
+    integral += r * r * charge_density_profile.at(i) * dr;
+  }
+  integral += .5 * r * r * dr * charge_density_profile.at(grid_count - 1);
+  // Spherical symmetry
+  integral *= 4. * M_PI;
+  return integral;
+}
+// _____________________________________________________________________________
 void FunctionalESMFSpherical::calc_derivative(
-    DataField<double>* functional_derivative) {
-  //
+    std::vector<DataFrame<1, double>>* functional_derivative) {
+  size_t i{0};
+  // Calculate the total charge density
+  calc_charge_densities();
+  // From the charge densities calculate the electrostatic potential
+  calc_potential();
+  // Calculate the derivative from the electrostatic potential
+  for (auto it = affected_species.begin(); it != affected_species.end(); ++it) {
+    i = it - affected_species.begin();
+    functional_derivative->at(*it) = valencies.at(i) * potential;
+  }
 }
 // _____________________________________________________________________________
 void FunctionalESMFSpherical::calc_bulk_derivative(
     std::vector<double>* bulk_derivative) {
-  //
+  // The bulk values of this functional's derivative are always zero
+  std::fill(bulk_derivative->begin(), bulk_derivative->end(), 0.);
 }
 // _____________________________________________________________________________
 double FunctionalESMFSpherical::calc_energy() {
-  //
-  return 0.;
+  DataFrame<1, double> integrand(grid_count);
+  double integral{0.};
+  double r{0.};
+  calc_charge_densities();
+  calc_potential();
+  integrand = potential * charge_density_profile;
+  // Trapezoidal integral rule, spherical integral
+  r = static_cast<double>(0 + 1) * dr;
+  integral += .5 * r * r * integrand.at(0) * dr;
+  for (size_t i = 1; i < grid_count - 1; ++i) {
+    r = static_cast<double>(i + 1) * dr;
+    integral += r * r * integrand.at(i) * dr;
+  }
+  integral += .5 * r * r * dr * integrand.at(grid_count - 1);
+  // Spherical symmetry
+  integral *= 4. * M_PI;
+  // 1/2 occuring in the MF functional
+  integral /= 2.;
+  return integral;
 }
-// _____________________________________________________________________________
 // _____________________________________________________________________________
--- src/functional_es_mf_spherical.hpp
+++ src/functional_es_mf_spherical.hpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2021 Moritz Bültmann <moritz.bueltmann@gmx.de>
+// SPDX-FileCopyrightText: 2022 Moritz Bültmann <moritz.bueltmann@gmx.de>
 // SPDX-License-Identifier: LGPL-3.0-or-later
 #ifndef SRC_FUNCTIONAL_ES_MF_SPHERICAL_HPP_
 #define SRC_FUNCTIONAL_ES_MF_SPHERICAL_HPP_
@@ -9,13 +9,12 @@
  *  class.
  *
  */
-// _____________________________________________________________________________
 // Includes
-#include <vector>
 #include "functional.hpp"  // NOLINT
+#include <vector>
+#include "data_frame.hpp"  // NOLINT
 #include "properties.hpp"  // NOLINT
-// Class forward declarations
-// _____________________________________________________________________________
+#include "radial_poisson_solver.hpp"  // NOLINT
 /** \brief This class calculates the elctrostatic mean field functional.
  *  
  *  This class contains the tools to calculate functional and functional
@@ -24,13 +23,25 @@
  */
 class FunctionalESMFSpherical : public Functional {
  public:
-  /** \brief Constructor
+  /** \brief Standard Constructor
+   *
+   */
+  FunctionalESMFSpherical();
+  /** \brief Manual Constructor
    *
    */
   FunctionalESMFSpherical(
-      const Properties& system_properties,
+      std::vector<DataFrame<1, double>>* density_profiles,
       const std::vector<Properties>& species_properties,
-      double** density_profile);
+      const Properties& system_properties,
+      std::vector<size_t> affected_species);
+  /** \brief Automated Constructor
+   *
+   */
+  FunctionalESMFSpherical(
+      std::vector<DataFrame<1, double>>* density_profiles,
+      const std::vector<Properties>& species_properties,
+      const Properties& system_properties);
   /** \brief Destructor
    *
    */
@@ -38,12 +49,13 @@ class FunctionalESMFSpherical : public Functional {
   /** \brief Calculate the functional derivatives
    *
    *  This function calculates the functional derivatives and updates the
-   *  corresponding internal array.
+   *  corresponding internal arrays.
    *
    *  It uses the functions:
    *  calc_weighted_densities(),
    */
-  virtual void calc_derivative(DataField<double>* functional_derivative);
+  virtual void calc_derivative(
+      std::vector<DataFrame<1, double>>* functional_derivative);
   /** \brief Calculate bulk derivatives
    *
    */
@@ -59,65 +71,83 @@ class FunctionalESMFSpherical : public Functional {
 
  private:
   /** \brief System length (radius of the sperical geometry)
-   *
    */
   double length;
   /** \brief Number of grid points
-   *
    */
   size_t grid_count;
+  /** \brief Bin width
+   */
+  double dr;
+  /** \brief Bin width in Fourier space
+   */
+  double dkr;
   /** \brief Number of species
-   *
    */
   size_t species_count;
   /** \brief Bjerrum length
-   *
    */
-  double bjerrum_length;
+  double bjerrum;
   /** \brief System temperature
-   *
    */
   double temperature;
   /** \brief Dielectric constant
-   *
    */
   double dielectric;
-  /** \brief Charge of the center sphere
-   *
+  /** \brief A list with indices of the affected species
    */
-  double center_charge;
-  /** \brief Hard sphere diameters
+  std::vector<size_t> affected_species;
+  /** \brief Valency of every species
    *
+   *  To be more general, valencies must be given as "double".
    */
   std::vector<double> valencies;
-  /** \brief Hard sphere diameters
-   *
-   */
-  std::vector<double> diameters;
   /** \brief Bulk densities
-   *
    */
   std::vector<double> bulk_densities;
   /** \brief Pointer to density profiles
-   *
    */
-  double** density_profile;
-  /** \brief Charge density profiles
-   *
+  std::vector<DataFrame<1, double>>* density_profiles_pointer;
+  /** \brief Total charge density profile
    */
-  double** charge_density_profile;
-  /** \brief Species-dependent electrostatic potentials
-   *
+  DataFrame<1, double> charge_density_profile;
+  /** \brief Right hand side of the Poisson equation
    */
-  double** species_potentials;
-  /** \brief Functional derivatives
-   *
+  DataFrame<1, double> poisson_rhs;
+  /** \brief Electrostatic potential (numerical solution of Poisson equation)
    */
+  DataFrame<1, double> potential;
+  /** \brief Poisson solver
+   *
+   *  This object contains the matrix representation of the numerical Poisson
+   *  equation.
+   */
+  RadialPoissonSolver* poisson_solver;
+  /** \brief Functional derivatives */
   double** functional_derivative;
-  /** \brief Calculate the weighted densities
-   *
+  /** \brief Extract the system Properties required for this functional */
+  void extract_system_properties(const Properties& system_properties);
+  /** \brief From two of the three electrical properties, the third on can be
+   *         calculated.
    */
-  void calc_weighted_densities();
+  void extract_electrical_properties(const Properties& system_properties);
+  /** \brief Extract the species specific Properties required for this
+   *         functional
+   */
+  void extract_species_properties(
+      const std::vector<Properties>& species_properties);
+  /** \brief Allocate memory for all DataFrame objects */
+  void initialize_all_data_frames();
+  /** \brief Initialize the RadialPoissonSolver object */
+  void initialize_poisson_solver();
+  /** \brief Calculate the net charge density profile and the rhs of the Poisson
+   *         equation.
+   */
+  void calc_charge_densities();
+  /** \brief From the charge densities calculate the electrostatic potential */
+  void calc_potential();
+  /** \brief Integration over charge densities to obtain net charge */
+  double calc_net_charge();
 
  protected:
 };
