23e7c9b525d50c700bdc64fdc218726d1dfe4392
moritz-bueltmann <moritz.bueltmann@physik.uni-freiburg.de>
2022-07-08 13:34:55 +0200

    Added another kind of iteration algorithm: Andersen mixing.

--- src/iterator.cpp
+++ src/iterator.cpp
@@ -3,6 +3,9 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 #include "iterator.hpp"  // NOLINT
 #include <cmath>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_multiroots.h>
+#include <limits>
 #include "data_frame.hpp"  // NOLINT
 #include "convergence_criterion.hpp"  // NOLINT
 #include "convergence_criterion_max_dev.hpp"  // NOLINT
@@ -54,11 +57,10 @@ void Iterator::clear_functionals() {
   bulk_derivatives.clear();
 }
 // _____________________________________________________________________________
-void Iterator::run(double mixing) {
+void Iterator::run_picard(double mixing) {
   double bulk_density{0.};
   size_t steps{0};
   size_t species_count{species_properties->size()};
-  std::vector<double> maximum_deviations(0);
   // Calculate bulk derivatives
   for (size_t i = 0; i < excess_functionals.size(); ++i) {
     excess_functionals.at(i)->calc_bulk_derivative(&bulk_derivatives.at(i));
@@ -82,6 +84,69 @@ void Iterator::run(double mixing) {
       // Mix the new solution with the old one
       density_profiles->at(i) = density_profiles->at(i) * (1. - mixing) +
           mixing * proposed_densities.at(i);
+    }
+  }
+}
+// _____________________________________________________________________________
+void Iterator::run_andersen(double mixing, size_t memory) {
+  double bulk_density{0.};
+  size_t steps{0};
+  size_t species_count{species_properties->size()};
+  size_t grid_count{proposed_densities.at(0).size()};
+  // A lot of memory is needed for all the past profiles
+  std::vector<DataFrame<1, double>> deviation_profiles(
+      species_count, DataFrame<1, double>(grid_count));
+  std::vector<std::vector<DataFrame<1, double>>> past_density_profiles(0);
+  std::vector<std::vector<DataFrame<1, double>>> past_proposed_densities(0);
+  std::vector<std::vector<DataFrame<1, double>>> past_density_deviations(0);
+  std::vector<std::vector<double>> scalar_products(0);
+  std::vector<double> alphas(0);
+  // Calculate bulk derivatives
+  for (size_t i = 0; i < excess_functionals.size(); ++i) {
+    excess_functionals.at(i)->calc_bulk_derivative(&bulk_derivatives.at(i));
+  }
+  // Iterator loop
+  while (!check_convergence_criteria()) {
+    ++steps;
+    // Calculate the functional derivatives.
+    for (size_t i = 0; i < excess_functionals.size(); ++i) {
+      excess_functionals.at(i)->calc_derivative(&functional_derivatives.at(i));
+    }
+    // Picard iteration scheme
+    for (size_t i = 0; i < species_count; ++i) {
+      species_properties->at(i).get_property("bulk density", &bulk_density);
+      // Calculate the right hand side of the update formula of cDFT.
+      proposed_densities.at(i) = bulk_density * exp_external_potentials->at(i);
+      for (size_t j = 0; j < functional_derivatives.size(); ++j) {
+        proposed_densities.at(i) *= exp(bulk_derivatives.at(j).at(i)) *
+            exp(-1. * functional_derivatives.at(j).at(i));
+        deviation_profiles.at(i) =
+            proposed_densities.at(i) - density_profiles->at(i);
+      }
+    }
+    // Append density profile, proposed density profile and deviation profile
+    // to history
+    past_density_profiles.push_back(*(density_profiles));
+    past_proposed_densities.push_back(proposed_densities);
+    past_density_deviations.push_back(deviation_profiles);
+    if (past_density_profiles.size() > memory) {
+      past_density_profiles.erase(past_density_profiles.begin());
+      past_proposed_densities.erase(past_proposed_densities.begin());
+      past_density_deviations.erase(past_density_deviations.begin());
+    }
+    // Calculate all possible scalar products between the density deviations
+    scalar_products = calc_scalar_products(past_density_deviations);
+    // Find the linear combination (alphas) that minimizes the norm
+    alphas = shortest_linear_combination(scalar_products);
+    // Update formula
+    for (size_t i = 0; i < species_count; ++i) {
+      density_profiles->at(i).zero();
+      for (size_t j = 0; j < past_density_profiles.size(); ++j) {
+        density_profiles->at(i) += (1. - mixing) * alphas.at(j) *
+            past_density_profiles.at(j).at(i);
+        density_profiles->at(i) += mixing * alphas.at(j) *
+            past_proposed_densities.at(j).at(i);
+      }
     }
   }
 }
@@ -122,5 +187,87 @@ bool Iterator::check_convergence_criteria() {
   }
   std::cout << "\"" << std::endl;
   return converged;
+}
+// _____________________________________________________________________________
+std::vector<std::vector<double>> Iterator::calc_scalar_products(
+    std::vector<std::vector<DataFrame<1, double>>>& profile_history) {
+  size_t hist_size{profile_history.size()};
+  size_t species_count{profile_history.at(0).size()};
+  size_t grid_count{profile_history.at(0).at(0).size()};
+  std::vector<std::vector<double>> scalar_products(
+      hist_size, std::vector<double>(hist_size));
+  double sum {0.};
+  for (size_t i = 0; i < hist_size; ++i) {
+    for (size_t j = i; j < hist_size; ++j) {
+      sum = 0.;
+      for (size_t k = 0; k < species_count; ++k) {
+        for (size_t l = 0; l < grid_count; ++l) {
+          sum += profile_history.at(i).at(k).at(l) * profile_history.at(j).at(k).at(l);
+        }
+      }
+      scalar_products.at(i).at(j) = scalar_products.at(j).at(i) = sum;
+    }
+  }
+  return scalar_products;
+}
+// _____________________________________________________________________________
+std::vector<double> Iterator::shortest_linear_combination(
+    std::vector<std::vector<double>>& scalar_products) {
+  size_t vector_count{scalar_products.size()};
+  size_t param_count{vector_count + 1};  // Lagrange multiplier gives +1
+  size_t iter{0};
+  int status{GSL_CONTINUE};
+  std::vector<double> alphas(vector_count);
+  // Initial guess
+  gsl_vector* x = gsl_vector_calloc(param_count);
+  for (size_t i = 0; i < vector_count; ++i) {
+    gsl_vector_set(x, i, 1. / static_cast<double>(vector_count));
+  }
+  // Find the root
+  const gsl_multiroot_fsolver_type* T = gsl_multiroot_fsolver_hybrids;
+  //const gsl_multiroot_fsolver_type* T = gsl_multiroot_fsolver_dnewton;
+  gsl_multiroot_fsolver* s = gsl_multiroot_fsolver_alloc (T, param_count);
+  gsl_multiroot_function f = {&anderson_f, param_count, &scalar_products};
+  gsl_multiroot_fsolver_set (s, &f, x);
+  while ((status == GSL_CONTINUE) && (iter++ < 1000)) {
+    status = gsl_multiroot_fsolver_iterate(s);
+    status = gsl_multiroot_test_residual (s->f, 1e-10);
+  }
+  for (size_t i = 0; i < vector_count; ++i) {
+    alphas.at(i) = gsl_vector_get(s->x, i);
+  }
+  return alphas;
+}
+// _____________________________________________________________________________
+int anderson_f (const gsl_vector* x, void* params, gsl_vector* f) {
+  std::vector<std::vector<double>> scalar_products =
+      *((std::vector<std::vector<double>>*) params);
+  size_t vector_count{scalar_products.size()};
+  double sum{0.};
+  double lambda{gsl_vector_get(x, vector_count)};
+  for (size_t i = 0; i < vector_count; ++i) {
+    sum = 0.;
+    for (size_t j = 0; j < vector_count; ++j) {
+      sum += gsl_vector_get(x, j) * scalar_products.at(j).at(i);
+    }
+    gsl_vector_set(f, i, 2. * sum - lambda);
+  }
+  sum = 0.;
+  for (size_t i = 0; i < vector_count; ++i) {
+    sum += gsl_vector_get(x, i);
+  }
+  gsl_vector_set(f, vector_count, sum - 1.);
+  return GSL_SUCCESS;
+}
+// _____________________________________________________________________________
+void print_state (size_t iter, gsl_multiroot_fsolver * s, size_t n) {
+  std::cout << "iter=" << iter << std::endl;
+  for (size_t i = 0; i < n; ++i) {
+    std::cout << "x[" << i << "]=" << gsl_vector_get(s->x, i) << std::endl;
+  }
+  for (size_t i = 0; i < n; ++i) {
+    std::cout << "f(x[" << i << "])=" << gsl_vector_get(s->f, 0) << std::endl;
+  }
+  std::cout << "**********************************************" << std::endl;
 }
 // _____________________________________________________________________________
